dc2d8f6afe4ec845e91af602026fa3e75cd7f8ce
ktatu <kaikkonentatu@gmail.com>
2023-05-28 02:44:40 +0300

    13.22

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,27 +1,10 @@
 const router = require("express").Router()
 const { Blog, User } = require("../models")
-const jwt = require("jsonwebtoken")
-const { SECRET } = require("../utils/config")
 const { Op } = require("sequelize")
+const tokenExtractor = require("../utils/tokenExtractor")
 
 const blogFinder = async (req, res, next) => {
     req.blog = await Blog.findByPk(req.params.id)
-    next()
-}
-
-const tokenExtractor = (req, res, next) => {
-    const authorization = req.get("authorization")
-
-    if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-        try {
-            req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-        } catch (error) {
-            return res.status(401).json({ error: "token invalid" })
-        }
-    } else {
-        return res.status(401).json({ error: "token missing" })
-    }
-
     next()
 }
 
@@ -59,7 +42,6 @@ router.get("/", async (req, res) => {
 })
 
 router.post("/", tokenExtractor, async (req, res) => {
-    console.log("-----------")
     try {
         const user = await User.findByPk(req.decodedToken.id)
         const blog = await Blog.create({ ...req.body, userId: user.id })
--- routes/readinglists.js
+++ routes/readinglists.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
-const { ReadingList } = require("../models")
+const { ReadingList, User } = require("../models")
+const tokenExtractor = require("../utils/tokenExtractor")
 
 router.post("/", async (req, res) => {
     try {
@@ -10,6 +11,30 @@ router.post("/", async (req, res) => {
         return res.json(readingList)
     } catch (error) {
         return res.status(401).json({ error: error })
+    }
+})
+
+router.put("/:id", tokenExtractor, async (req, res) => {
+    try {
+        if (req.body.read === true) {
+            const readingList = await ReadingList.findByPk(req.params.id, {
+                include: { model: User },
+            })
+            const user = await User.findByPk(req.decodedToken.id)
+
+            if (user.id !== readingList.user.id) {
+                throw new Error("Invalid credentials")
+            }
+
+            readingList.read = true
+            await readingList.save()
+
+            return res.status(201).end()
+        }
+        throw new Error("Invalid parameters")
+    } catch (error) {
+        console.log(error)
+        return res.status(401).json({ error: error.message })
     }
 })
 
